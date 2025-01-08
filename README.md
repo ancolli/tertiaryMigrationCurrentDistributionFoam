ba2113e5996cb839d306cd867e3b5146aa02ad36
Alejandro Colli <ancolli@gmail.com>
2025-01-08 14:01:20 +0100

    compatibility with 2312 version

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # v1.0.0
 
-Cite as: Ind. Eng. Chem. Res. 2021, 60, 32, 11927–11941
+Cite as: Ind. Eng. Chem. Res. 60 (2021) 11927
 DOI: https://doi.org/10.1021/acs.iecr.1c01884
 
 # tertiaryMigrationCurrentDistributionFoam
-It is described how to simulate tertiary current distribution (including the effect of migration of ions) in electrochemical reactors with the help of OpenFOAM, the developed solver potentialMigrConcentrationFoam and the BC as codedMixed. It shows how to pre-process, run and post-process a basic case involving in a 2D domain. 
-The proposed strategy allows to have as an input k-th electrochemical reactions per electrode. Fully functional in OpenFOAM version 8.
+The process of simulating tertiary current distribution, including the effect of ion migration, in electrochemical reactors is described using OpenFOAM, the custom-developed solver potentialMigrConcentrationFoam, and the codedMixed boundary condition. The workflow demonstrates how to pre-process, run, and post-process a basic case in a 2D domain.
+
+The proposed strategy supports the input of kk-th electrochemical reactions per electrode and is fully functional in OpenFOAM version 8 and version 2312.
 
 # Disclaimer
 This offering is not approved or endorsed by OpeFOAM Foundation, producer and distributor of the OpenFOAM software via www.openfoam.org.
---
+++ applications/utilities/postProcessing/OpenFOAM2312/speciesMigrationWallFlux/speciesMigrationWallFlux.C
@@ -0,0 +1,372 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2017 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "speciesMigrationWallFlux.H"
+
+#include "surfaceInterpolate.H"
+#include "fvcSnGrad.H"
+#include "wallPolyPatch.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace functionObjects
+{
+    defineTypeNameAndDebug(speciesMigrationWallFlux, 0);
+    addToRunTimeSelectionTable(functionObject, speciesMigrationWallFlux, dictionary);
+}
+}
+
+void Foam::functionObjects::speciesMigrationWallFlux::writeFileHeader(Ostream& os) const//void Foam::functionObjects::speciesMigrationWallFlux::writeFileHeader(const label i)
+{
+    // Add headers to output data
+    writeHeader(os, "Wall flux");//writeHeader(file(), "Wall flux");
+    writeCommented(os, "Time");//writeCommented(file(), "Time");
+    writeTabbed(os, "patch");//writeTabbed(file(), "patch");
+    writeTabbed(os, "min");//writeTabbed(file(), "min");
+    writeTabbed(os, "max");//writeTabbed(file(), "max");
+    writeTabbed(os, "integral");//writeTabbed(file(), "integral");
+    os << endl;//file() << endl;
+}
+
+void Foam::functionObjects::speciesMigrationWallFlux::calcFlux //void
+(
+    const volScalarField& fi_,
+    const volScalarField& C_, 
+    volScalarField& speciesMigrationWallFlux
+)
+
+{
+    
+    surfaceScalarField flux
+    (
+        nu_e_/nu_*F_*D_*fvc::snGrad(C_) + nu_e_/nu_*pow(F_,2)*u_*z_*fvc::interpolate(C_)*fvc::snGrad(fi_) // gradients are defined inwards
+    );
+
+    volScalarField::Boundary& speciesMigrationWallFluxBf =
+        speciesMigrationWallFlux.boundaryFieldRef();
+
+    const surfaceScalarField::Boundary& fluxBf =
+        flux.boundaryField();
+
+    forAll(speciesMigrationWallFluxBf, patchi)
+    {
+        speciesMigrationWallFluxBf[patchi] = fluxBf[patchi];
+    }
+}
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+
+Foam::functionObjects::speciesMigrationWallFlux::speciesMigrationWallFlux
+(
+    const word& name,
+    const Time& runTime,
+    const dictionary& dict
+)
+:
+    fvMeshFunctionObject(name, runTime, dict),
+
+    //logFiles(obr_, name),
+    writeFile(obr_, name, typeName, dict),//writeLocalObjects(obr_, log),
+    patchSet_() //,
+    //fvOptions_(mesh_),
+    
+/*    fi_
+    (
+       IOobject
+       (
+           "fi",
+           mesh_.time().timeName(),
+           mesh_,
+           IOobject::NO_READ,//MUST_READ,
+           IOobject::NO_WRITE
+       ),
+       mesh_
+       //dimensionedScalar("0", dimensionSet(0, 0, 0, 0, 0), 0.0)
+      
+    ),
+
+    C_
+    (
+       IOobject
+       (
+           type(),//"C_" + CName_,
+           mesh_.time().timeName(),
+           mesh_,
+           IOobject::NO_READ,//MUST_READ,
+           IOobject::NO_WRITE
+       ),
+       mesh_,
+       dimensionedScalar("0", C_.dimensions(), 0.0)//dimensionedScalar("0", dimensionSet(0, 0, 0, 0, 0), 0.0)//
+    )*/
+    
+       
+{
+    volScalarField* speciesMigrationWallFluxPtr
+    (
+	new volScalarField
+        (
+            IOobject
+            (
+                type(),
+                mesh_.time().timeName(),
+                mesh_,
+                IOobject::NO_READ,
+                IOobject::NO_WRITE
+            ),
+            mesh_,
+            dimensionedScalar("0", dimMoles/dimVolume/dimTime*dimLength, 0.0)
+        )
+    );
+
+
+    
+    mesh_.objectRegistry::store(speciesMigrationWallFluxPtr);
+    
+    read(dict);
+    //resetName(typeName);
+    //resetLocalObjectName(typeName);
+    writeFileHeader(file());
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::functionObjects::speciesMigrationWallFlux::~speciesMigrationWallFlux()
+{}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+bool Foam::functionObjects::speciesMigrationWallFlux::read(const dictionary& dict)
+{
+
+
+    fvMeshFunctionObject::read(dict);
+    writeFile::read(dict);//writeLocalObjects::read(dict);
+     
+    const polyBoundaryMesh& pbm = mesh_.boundaryMesh();
+
+    patchSet_ =
+        mesh_.boundaryMesh().patchSet
+        (
+            wordReList(dict.lookupOrDefault("patches", wordReList()))
+        );
+
+    Info<< type() << " " << name() << ":" << nl;
+
+    if (patchSet_.empty())
+    {
+        forAll(pbm, patchi)
+        {
+            if (isA<wallPolyPatch>(pbm[patchi]))
+            {
+                patchSet_.insert(patchi);
+            }
+        }
+
+        Info<< "    processing all wall patches" << nl << endl;
+    }
+    else
+    {
+        Info<< "    processing wall patches: " << nl;
+        labelHashSet filteredPatchSet;
+        forAllConstIter(labelHashSet, patchSet_, iter)
+        {
+            label patchi = iter.key();
+            if (isA<wallPolyPatch>(pbm[patchi]))
+            {
+                filteredPatchSet.insert(patchi);
+                Info<< "        " << pbm[patchi].name() << endl;
+            }
+            else
+            {
+                WarningInFunction
+                    << "Requested wall flux on non-wall boundary "
+                    << "type patch: " << pbm[patchi].name() << endl;
+            }
+        }
+
+        Info<< endl;
+
+        patchSet_ = filteredPatchSet;
+    }
+
+
+    /*if (dict.found("fvOptions"))
+    {
+        fvOptions_.reset(dict.subDict("fvOptions"));
+    }*/
+
+    
+    dict.lookup("CName") >> CName_; 
+
+    IOdictionary speciesProperties
+    (
+    	IOobject
+    	(
+        	"speciesProperties",
+        	mesh_.time().constant(),//runTime.constant(),
+        	mesh_,
+        	IOobject::MUST_READ,//MUST_READ_IF_MODIFIED,
+        	IOobject::NO_WRITE
+    	)
+    );
+
+
+    dictionary speciesName
+    (
+    	speciesProperties.subDict(CName_)
+    );
+
+    speciesName.lookup("D") >> D_;
+    speciesName.lookup("u") >> u_;
+    speciesName.lookup("z") >> z_;
+
+    IOdictionary transportProperties
+    (
+    	IOobject
+    	(
+        	"transportProperties",
+		mesh_.time().constant(),//mesh_.time().timeName(),
+        	mesh_,
+        	IOobject::MUST_READ,
+        	IOobject::NO_WRITE
+   	)
+   );
+
+   transportProperties.lookup("F") >> F_;
+
+   transportProperties.lookup("nu_e") >> nu_e_;
+
+   transportProperties.lookup("nu_" + CName_) >> nu_;
+
+   return true;
+}
+
+
+bool Foam::functionObjects::speciesMigrationWallFlux::execute()
+{
+    volScalarField& speciesMigrationWallFlux = lookupObjectRef<volScalarField>(type());
+
+    volScalarField fi_
+    (
+       IOobject
+       (
+          "fi",
+           mesh_.time().timeName(),
+           mesh_,
+           IOobject::MUST_READ,
+           IOobject::NO_WRITE
+       ),
+       mesh_   
+    );
+
+    volScalarField C_
+    (
+       IOobject
+       (
+           "C_" + CName_,
+           mesh_.time().timeName(),
+           mesh_,
+           IOobject::MUST_READ,
+           IOobject::NO_WRITE
+       ),
+       mesh_ 
+    );
+
+    calcFlux(fi_, C_, speciesMigrationWallFlux);
+
+    return true;
+}
+
+
+/*bool Foam::functionObjects::speciesMigrationWallFlux::end()
+{
+
+    return true;
+}*/
+
+
+bool Foam::functionObjects::speciesMigrationWallFlux::write()
+{
+    //Log << type() << " " << name() << " write:" << nl;
+
+    //writeLocalObjects::write();
+
+    //logFiles::write();
+
+    //const volScalarField& speciesMigrationWallFlux =
+        //obr_.lookupObject<volScalarField>(type());
+        
+    const volScalarField& speciesMigrationWallFlux = lookupObject<volScalarField>(type());
+
+    Log << type() << " " << name() << " write:" << nl
+        << "    writing field " << speciesMigrationWallFlux.name() << endl;
+
+    speciesMigrationWallFlux.write();
+
+    const fvPatchList& patches = mesh_.boundary();
+
+    const surfaceScalarField::Boundary& magSf =
+        mesh_.magSf().boundaryField();
+
+    for (const label patchi : patchSet_)//forAllConstIter(labelHashSet, patchSet_, iter)
+    {
+        //label patchi = iter.key();
+        const fvPatch& pp = patches[patchi];
+
+        const scalarField& hfp = speciesMigrationWallFlux.boundaryField()[patchi];
+
+        const scalar minHfp = gMin(hfp);
+        const scalar maxHfp = gMax(hfp);
+        const scalar integralHfp = gSum(magSf[patchi]*hfp)/gSum(magSf[patchi]);
+	const scalar current = gSum((magSf[patchi]*hfp)); //absolute value
+
+        if (Pstream::master())
+        {
+            writeCurrentTime(file());
+            file()
+               // << mesh_.time().value()
+                << tab << pp.name()
+                << tab << minHfp
+                << tab << maxHfp
+                << tab << integralHfp
+		<< tab << current
+                << endl;
+        }
+
+        Log << "    min/max/average/current(" << pp.name() << ") = "
+            << minHfp << ", " << maxHfp << ", " << integralHfp << ", " << current << endl;
+    }
+
+    Log << endl;
+
+    return true;
+}
+
+
+// ************************************************************************* //
